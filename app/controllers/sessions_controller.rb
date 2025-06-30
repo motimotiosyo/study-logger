15dd8656e990e55e50644fcf460c9cfb247aa431
田畑友作 <kuppa0713@icloud.com>
2025-07-01 04:02:58 +0900

    chore:必要ファイルの作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "profiles/show"
+  get "profiles/edit"
+  get "sessions/index"
+  get "sessions/new"
+  get "sessions/edit"
+  get "dashboard/index"
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
