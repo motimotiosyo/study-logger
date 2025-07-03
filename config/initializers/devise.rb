25d3f186ba0848f0c3674f856e42f25aa4c17c23
田畑友作 <kuppa0713@icloud.com>
2025-07-03 23:31:11 +0900

    fix: 本番環境エラー対応2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "noreply@study-logger-nc1w.onrender.com"
+  config.mailer_sender = "study.logger.app@gmail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
